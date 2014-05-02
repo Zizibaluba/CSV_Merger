e34c5305ee9245e8aa57eb8d409cd4acfe2266af
Zizibaluba <zizibaluba@gmail.com>
2014-05-01 22:30:05 -0500

    User naming UI

--- csvMerger.py
+++ csvMerger.py
@@ -26,8 +26,8 @@ def processCsv(filename):
 	return csvDic
 
 #Writes all the content into a combinedBook.csv
-def writeCsvFile(csvLines):
-	with open('combinedBook.csv', "w") as f:
+def writeCsvFile(csvLines, filename):
+	with open(filename, "w") as f:
 		for line in csvLines:
 			for item in line:
 				#Takes out extra commas in currency and names
@@ -55,29 +55,54 @@ def dirCheck(directory):
 	while (len(directory) < 1):
 		directory = raw_input("Please input a directory or restart program with a directory argument:\n")
 	if (os.path.isdir(directory) is True):
-		raw_input("The CSV is in is: " + str(directory) + "\nPress enter to continue or end program with Ctrl + C.\n")
+		raw_input("\nThe CSV is in is: " + str(directory) + "\n\nPress enter to continue or end program with Ctrl + C.\n")
 	else:
 		print("The directory does not exist. Please try again. Ending Program")
 		sys.exit()
 	return directory
 
 #Asks the user if they want to include the file name to the last column of each of the rows they're merging
-def wantName():
-	includeName = raw_input("Do you want the filename to be included at the last column in each row? Y/N:\n")
+def choice(choiceItem):
+	includeItemChoice = raw_input("Do you want " + choiceItem + " Y/N:\n")
 	while True:
-		if (len(includeName) > 1):
-			includeName = raw_input("Not a valid answer. Please only use Y or N.\n")
+		if (len(includeItemChoice) > 1):
+			includeItemChoice = raw_input("\nNot a valid answer. Please only use Y or N.\n")
 		else:
-			if re.match("y|Y", includeName) is not None:
+			if re.match("y|Y", includeItemChoice) is not None:
 				return True
-			elif re.match("n|N", includeName) is not None:
+			elif re.match("n|N", includeItemChoice) is not None:
 				return False
 			else:
-				includeName = "bad"
+				includeItemChoice = "bad"
+
+#Gives the user a chance to name their file, continues until a suitable name is created
+def nameFile(directory):
+	while True:
+		filename = raw_input("\nWhat would you like the merged file to be called? \n(If none is entered, the file will be called mergedCSV)\n")
+		if (filename == ""):
+			filename = "mergedCSV.csv"
+		else:
+			filename += ".csv"
+		fileExist = checkExist(filename, directory)
+		if fileExist == True:
+			overwriteYN = choice("to overwrite the file?")
+			if(overwriteYN == False):
+				continue
+			else:
+				return filename
+		else:
+			return filename
+
+#Checks if a file already exist within the directory
+def checkExist(name, directory):
+	if (os.path.exists(directory + "/" + name) == True):
+		print "The file already exists."
+		return True
+	return False			
 
 if __name__ == "__main__":
 	csvDic = []
-	if len(sys.argv) > 2:
+	if len(sys.argv) > 1:
 		directory = sys.argv[1]
 	else:
 		directory = ""
@@ -86,7 +111,7 @@ if __name__ == "__main__":
 	#Gets all csv files from the current directory
 	csvFilenames = glob.glob("*.csv")
 	#Ask user if they want to include the filename in the CSV output
-	includeName = wantName()
+	includeName = choice("the filenames to be included at the last column in each row?")
 	#Takes all csv files and processes then write everything in memory to the combined csv file
 	for filename in csvFilenames:
 		csvDicPart = processCsv(filename)
@@ -95,5 +120,6 @@ if __name__ == "__main__":
 			if (includeName == True):
 				row.append(minusDotAfter(filename))
 			csvDic.append(row)
-	writeCsvFile(csvDic)
+	filename = nameFile(directory)
+	writeCsvFile(csvDic, filename)
 	print("\n=====\nDone!\n=====\n")
